fa71f8f0c174e9c1df89eb24856c222458bd9c74
Harritone <yourrasik@gmail.com>
2020-10-28 23:13:43 +1000

    Created header and footer partials and included on the index and about actions

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,14 +1,19 @@
-<?php 
-class Pages  extends Controller {
-  public function __construct() {
-  }
+<?php
+class Pages extends Controller
+{
+    public function __construct()
+    {
+    }
 
-  public function index() {
- $data = ['title' => 'Welcome'];
-    $this->view('pages/index', $data);
-  }
+    public function index()
+    {
+        $data = ['title' => 'Welcome'];
+        $this->view('pages/index', $data);
+    }
 
-  public function about() {
-    $this->view('pages/about');
-  }
+    public function about()
+    {
+        $data = ['title' => 'About Us'];
+        $this->view('pages/about', $data);
+    }
 }
