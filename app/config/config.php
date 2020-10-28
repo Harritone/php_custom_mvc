7b71ec2c39e4c4134b98ba57a756644736b39da0
Harritone <yourrasik@gmail.com>
2020-10-28 22:54:28 +1000

    Implemented autoload libraries in the bootstrap.php and setted up config file

---
+++ app/config/config.php
@@ -0,0 +1,7 @@
+<?php
+// App root
+define('APPROOT', dirname(dirname(__FILE__)));
+// URL Root
+define('URLROOT', 'https://localhost/projects/traversymvc');
+// Site name
+define('SITENAME', 'TraversyMVC');
